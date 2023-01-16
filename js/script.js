226375ce7e0ed617201d813160d8d1b47f180463
Raghav Agarwal <94176162+raghammer1@users.noreply.github.com>
2023-01-16 18:11:43 +1100

    1st try on making webpage faster

--- js/script.js
+++ js/script.js
@@ -24,61 +24,61 @@ headerObserver.observe(header);
 /////////////// //////////////////////////////////
 // Creating smooth scrolling
 
-const homebtn = document.querySelector('.home');
-const aboutbtn = document.querySelector('.about');
-const expbtn = document.querySelector('.experience');
-const skillbtn = document.querySelector('.skills');
-const projbtn = document.querySelector('.projects');
-const homeSection = document.querySelector('#hi');
-const featureSec = document.querySelector('#about-me');
-const expSec = document.querySelector('#experience');
-const skillsSec = document.querySelector('#section--2');
-const projectSec = document.querySelector('#projects');
+// const homebtn = document.querySelector('.home');
+// const aboutbtn = document.querySelector('.about');
+// const expbtn = document.querySelector('.experience');
+// const skillbtn = document.querySelector('.skills');
+// const projbtn = document.querySelector('.projects');
+// const homeSection = document.querySelector('#hi');
+// const featureSec = document.querySelector('#about-me');
+// const expSec = document.querySelector('#experience');
+// const skillsSec = document.querySelector('#section--2');
+// const projectSec = document.querySelector('#projects');
 
-homebtn.addEventListener('click', function (e) {
-  const s1coords = homeSection.getBoundingClientRect();
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// homebtn.addEventListener('click', function (e) {
+//   const s1coords = homeSection.getBoundingClientRect();
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+//   });
+// });
 
-aboutbtn.addEventListener('click', function (e) {
-  const s1coords = featureSec.getBoundingClientRect();
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// aboutbtn.addEventListener('click', function (e) {
+//   const s1coords = featureSec.getBoundingClientRect();
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+//   });
+// });
 
-expbtn.addEventListener('click', function (e) {
-  const s1coords = expSec.getBoundingClientRect();
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// expbtn.addEventListener('click', function (e) {
+//   const s1coords = expSec.getBoundingClientRect();
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+//   });
+// });
 
-skillbtn.addEventListener('click', function (e) {
-  const s1coords = skillsSec.getBoundingClientRect();
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// skillbtn.addEventListener('click', function (e) {
+//   const s1coords = skillsSec.getBoundingClientRect();
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+//   });
+// });
 
-projbtn.addEventListener('click', function (e) {
-  const s1coords = projectSec.getBoundingClientRect();
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
-});
+// projbtn.addEventListener('click', function (e) {
+//   const s1coords = projectSec.getBoundingClientRect();
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: 'smooth',
+//   });
+// });
 
 /////////////// //////////////////////////////////
 //SKILLS//SKILLS//
@@ -106,23 +106,23 @@ tabsContainer.addEventListener('click', function (e) {
 });
 /////////////// //////////////////////////////////
 //REVEALING SECTION//REVEALINGSECTIONS
-const allSections = document.querySelectorAll('.section-new');
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
+// const allSections = document.querySelectorAll('.section-new');
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
 
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
-};
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-  behavior: 'smooth',
-});
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove('section--hidden');
+//   observer.unobserve(entry.target);
+// };
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.1,
+//   behavior: 'smooth',
+// });
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add('section--hidden');
+// });
 
 /////////////// //////////////////////////////////
 //lazy loading images //
