385e0b7b82f80f45cb63f7ab476eb26ab36341c6
Raghav Agarwal <94176162+raghammer1@users.noreply.github.com>
2023-01-16 18:19:59 +1100

    I think the slow problem was because of
    I think the slow problem was because of revealing elements part

--- js/script.js
+++ js/script.js
@@ -24,61 +24,61 @@ headerObserver.observe(header);
 /////////////// //////////////////////////////////
 // Creating smooth scrolling
 
-// const homebtn = document.querySelector('.home');
-// const aboutbtn = document.querySelector('.about');
-// const expbtn = document.querySelector('.experience');
-// const skillbtn = document.querySelector('.skills');
-// const projbtn = document.querySelector('.projects');
-// const homeSection = document.querySelector('#hi');
-// const featureSec = document.querySelector('#about-me');
-// const expSec = document.querySelector('#experience');
-// const skillsSec = document.querySelector('#section--2');
-// const projectSec = document.querySelector('#projects');
+const homebtn = document.querySelector('.home');
+const aboutbtn = document.querySelector('.about');
+const expbtn = document.querySelector('.experience');
+const skillbtn = document.querySelector('.skills');
+const projbtn = document.querySelector('.projects');
+const homeSection = document.querySelector('#hi');
+const featureSec = document.querySelector('#about-me');
+const expSec = document.querySelector('#experience');
+const skillsSec = document.querySelector('#section--2');
+const projectSec = document.querySelector('#projects');
 
-// homebtn.addEventListener('click', function (e) {
-//   const s1coords = homeSection.getBoundingClientRect();
-//   window.scrollTo({
-//     left: s1coords.left + window.pageXOffset,
-//     top: s1coords.top + window.pageYOffset,
-//     behavior: 'smooth',
-//   });
-// });
+homebtn.addEventListener('click', function (e) {
+  const s1coords = homeSection.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
 
-// aboutbtn.addEventListener('click', function (e) {
-//   const s1coords = featureSec.getBoundingClientRect();
-//   window.scrollTo({
-//     left: s1coords.left + window.pageXOffset,
-//     top: s1coords.top + window.pageYOffset,
-//     behavior: 'smooth',
-//   });
-// });
+aboutbtn.addEventListener('click', function (e) {
+  const s1coords = featureSec.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
 
-// expbtn.addEventListener('click', function (e) {
-//   const s1coords = expSec.getBoundingClientRect();
-//   window.scrollTo({
-//     left: s1coords.left + window.pageXOffset,
-//     top: s1coords.top + window.pageYOffset,
-//     behavior: 'smooth',
-//   });
-// });
+expbtn.addEventListener('click', function (e) {
+  const s1coords = expSec.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
 
-// skillbtn.addEventListener('click', function (e) {
-//   const s1coords = skillsSec.getBoundingClientRect();
-//   window.scrollTo({
-//     left: s1coords.left + window.pageXOffset,
-//     top: s1coords.top + window.pageYOffset,
-//     behavior: 'smooth',
-//   });
-// });
+skillbtn.addEventListener('click', function (e) {
+  const s1coords = skillsSec.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
 
-// projbtn.addEventListener('click', function (e) {
-//   const s1coords = projectSec.getBoundingClientRect();
-//   window.scrollTo({
-//     left: s1coords.left + window.pageXOffset,
-//     top: s1coords.top + window.pageYOffset,
-//     behavior: 'smooth',
-//   });
-// });
+projbtn.addEventListener('click', function (e) {
+  const s1coords = projectSec.getBoundingClientRect();
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
 
 /////////////// //////////////////////////////////
 //SKILLS//SKILLS//
